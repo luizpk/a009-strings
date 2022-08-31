8471acb0fa4e1574dc2ef385522835e0f889ff69
LuizPK <luizpkpinheiro@gmail.com>
2022-08-31 14:08:54 -0300

    Resolvendo exercício2

--- exercicio2/script.js
+++ exercicio2/script.js
@@ -1,1 +1,8 @@
 const minhaString = "Eu não sou supersticioso, mas sou um pouco ________.      ";
+const minhaString2 = minhaString.trim()
+
+console.log(`${minhaString.length}
+${minhaString2.length}`)
+
+console.log(minhaString2.replace("________","sticioso")
+)
